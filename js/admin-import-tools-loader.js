ef1a2b711327d241b514691907478721473c6677
complexdd-rgb <complexdd@hotmail.co.uk>
2026-08-13 22:57:46 +0100

    Add unified Automatic Creator loader

---
+++ js/admin-import-tools-loader.js
@@ -0,0 +1,14 @@
+/* FPL Challenge Studio · admin import-tools loader v16.0.2 */
+(() => {
+  "use strict";
+  const load = (src, done) => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.async = false;
+    if (done) script.addEventListener("load", done, { once: true });
+    document.head.appendChild(script);
+  };
+  load("js/admin-import-tools-base.js?v=16.0.1-unified1", () => {
+    load("js/career-shape-unified-generator.js?v=1.0.0");
+  });
+})();
